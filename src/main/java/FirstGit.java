fad9f88e2d713f848b70eca9ae4b4cfb64960063
nevmerzhytskiy20008-tech <nevmerzhytskiy20008@gmail.com>
2025-09-18 14:42:42 +0300

    Update FirstGit.java

--- src/main/java/FirstGit.java
+++ src/main/java/FirstGit.java
@@ -1,6 +1,6 @@
 public class FirstGit {
     public static void main(String[] args) {
         System.out.println("GIT");
-        System.out.println("LEARMING");
+        System.out.println("LEARMING!");
     }
 }
