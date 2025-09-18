4ed0dae7e5da9300b2bb1a7e041afa9f4fc4a873
CJIOH <nevmerzhytskiy20008@gamil.com>
2025-09-18 15:42:40 +0300

    change sout

--- src/main/java/FirstGit.java
+++ src/main/java/FirstGit.java
@@ -4,5 +4,6 @@ public class FirstGit {
         System.out.println("LEARMING!!");
         System.out.println("I'm happy");
         System.out.println("!!!!!");
+        System.out.println("!!!!!");
     }
 }
