0bb0aa1f02ad18375246154b553263d1f82673cc
CJIOH <nevmerzhytskiy20008@gamil.com>
2025-09-18 15:37:30 +0300

    del sout

--- src/main/java/FirstGit.java
+++ src/main/java/FirstGit.java
@@ -2,8 +2,6 @@ public class FirstGit {
     public static void main(String[] args) {
         System.out.println("GIT DEVELOP");
         System.out.println("LEARMING!!");
-        System.out.println("It's terribly difficult");
-        System.out.println("LEARMING!");
         System.out.println("I'm happy");
         System.out.println("!!!!!");
     }
