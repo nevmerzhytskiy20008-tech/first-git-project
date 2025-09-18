e843815603dbef0e2445442f4fe58b1df56f6042
nevmerzhytskiy20008-tech <nevmerzhytskiy20008@gmail.com>
2025-09-18 14:51:37 +0300

    Update FirstGit.java
    Add sout

--- src/main/java/FirstGit.java
+++ src/main/java/FirstGit.java
@@ -1,6 +1,7 @@
 public class FirstGit {
     public static void main(String[] args) {
         System.out.println("GIT");
-        System.out.println("LEARMING!!");
+        System.out.println("LEARMING!");
+        System.out.println("I'm happy");
     }
 }
