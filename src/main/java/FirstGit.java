d4ac54122a3c0718dc633629f2d7c591aa17b5ac
CJIOH <nevmerzhytskiy20008@gamil.com>
2025-09-18 15:10:05 +0300

    Add sout

--- src/main/java/FirstGit.java
+++ src/main/java/FirstGit.java
@@ -2,5 +2,6 @@ public class FirstGit {
     public static void main(String[] args) {
         System.out.println("GIT");
         System.out.println("LEARMING!!");
+        System.out.println("It's terribly difficult");
     }
 }
